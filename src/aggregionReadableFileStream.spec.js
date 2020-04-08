792bba3b286d7aa392031b18c4e322997e9464be
Denis Bezrukov <anthrax63@gmail.com>
2020-04-08 19:03:18 +0300

    remove agg-bundle dependency

--- src/aggregionReadableFileStream.spec.js
+++ src/aggregionReadableFileStream.spec.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 const digestStream = require('digest-stream');
 const passStream = require('pass-stream');
 const AggregionBundle = require('agg-bundle');
